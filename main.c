086701511198c411f2ae460d0c68d9e9fa2e947c
migoox <56317134+migoox@users.noreply.github.com>
2021-11-20 22:41:51 +0100

    multiple file output added

--- main.c
+++ main.c
@@ -1,330 +1,392 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define MAX_FILE_NAME_SIZE 64
-#define MAX_ERROR_MSG_SIZE 64
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <windows.h>
 
 #include "ansnum.h"
 #include "ansoperations.h"
 
+#define MAX_FILE_NAME_SIZE 64
+#define MAX_ERROR_MSG_SIZE 64
+
+bool do_operation(FILE* input_file, FILE* output_file, int operation_counter);
+
+void create_current_output_file_name(char* input_file_path, char* output_file_path_container, int operation_counter);
+
 int main()
 {
-	// file names
-	char file_name[MAX_FILE_NAME_SIZE];
-	char result_file_name[MAX_FILE_NAME_SIZE];
+	char input_file_path[MAX_FILE_NAME_SIZE];
+	char output_file_path[MAX_FILE_NAME_SIZE];
+	char output_type_choice;
 
-	// current operation
-	char operation;
+	FILE* input_file;
+	FILE* current_output_file;
 
-	// helper container
-	char c;
-
-	// input numeral systems
-	int numeral_system1;
-	int numeral_system2;
+	bool single_file_output;
 
 	int operation_counter;
 
-	// clock
+	while (true)
+	{
+		operation_counter = 0;
+		single_file_output = false;
+
+		// file name input
+		printf("Calculator: Enter txt file name(without extension) or type \".\" to shut down the program.\n>>");
+		scanf("%s", input_file_path);
+
+		// exit case
+		if (input_file_path[0] == '.')
+			break;
+
+		// repairing file name input
+		strcat(input_file_path, ".txt\0");
+
+		// opening input file
+		input_file = fopen(input_file_path, "r");
+
+		// checking if the file exists
+		if (input_file == NULL)
+		{
+			printf("Calculator: File doesn't exist.\n");
+			continue;
+		}
+
+		// single file output choice
+		printf("Calculator: Do you want single file output? Y/N\n>>");
+		scanf(" %c", &output_type_choice);
+
+		if (output_type_choice == 'Y' || output_type_choice == 'y')
+			single_file_output = true;
+
+		// single output case
+		if (single_file_output)
+		{
+			// prepare output name
+			strncpy(output_file_path, input_file_path, strlen(input_file_path) - 4);
+			output_file_path[strlen(input_file_path) - 4] = '\0';
+			strcat(output_file_path, "_result.txt\0");
+
+			// open(create) output file
+			current_output_file = fopen(output_file_path, "w");
+
+			// do operations loop
+			do
+			{
+				operation_counter++;
+			} while (do_operation(input_file, current_output_file, operation_counter));
+
+			// close output file
+			fclose(current_output_file);
+		}
+		else
+		{
+			while (fgetc(input_file) != EOF)
+			{
+				// go back
+				fseek(input_file, -1, SEEK_CUR);
+
+				operation_counter++;
+
+				create_current_output_file_name(input_file_path, output_file_path, operation_counter);
+
+				// do operation and save it in new file
+				current_output_file = fopen(output_file_path, "w");
+
+				do_operation(input_file, current_output_file, operation_counter);
+
+				fclose(current_output_file);
+			}
+		}
+		// close input file
+		fclose(input_file);
+	}
+	return 0;
+}
+
+bool do_operation(FILE* input_file, FILE* output_file, int operation_counter)
+{
+	char current_operation;
+	char operation_error_msg[MAX_ERROR_MSG_SIZE];
+	char temp;
+
+	bool operation_error;
+
+	int numeral_system;
+	int numeral_system2;
+
 	float clock_start;
 	float clock_end;
 	float elapsed_time;
 
-	// file reading error status
-	bool file_error;
-
-	// operation error status
-	bool operation_error;
-
-	// operation error messages
-	char operation_error_incorrect_system[MAX_ERROR_MSG_SIZE] = 
-		"ERROR: Numeral system should be between 2 and 16";
-
-	char operation_error_msg[MAX_ERROR_MSG_SIZE];
-
-	// input and output file
-	FILE* ifile;
-	FILE* ofile;
-
-	// number containers
 	ANS_Num num1;
 	ANS_Num num2;
 	ANS_Num result;
 	ANS_Num num;
 
-	while (true)
+	// initialize error flag
+	operation_error = false;
+
+	current_operation = fgetc(input_file);
+
+	if (current_operation == EOF)
+		return false;
+
+	printf("Calculator: Working on operation %d...", operation_counter);
+
+	// print current operation in output_file
+	fprintf(output_file, "%c", current_operation);
+
+	// arithmetic operations
+	if (current_operation == '+' || current_operation == '*' || current_operation == '/' || current_operation == '%' || current_operation == '^')
 	{
-		// initialize values
-		operation_counter = 1;
-		file_error = false;
+		fprintf(output_file, "%c", fgetc(input_file));
 
-		// choosing the file
-		printf("Calculator: Enter txt file name(without extension) or type \".\" to shut down the program.\n>>");
-		scanf("%s", file_name);
+		fscanf(input_file, "%d", &numeral_system);
+		fprintf(output_file, "%d", numeral_system);
 
-		// exit case
-		if (file_name[0] == '.')
-			break;
+		// init number containers(allocate memory)
+		ANS_init_cap(&num1, 2, numeral_system);
+		ANS_init_cap(&num2, 2, numeral_system);
+		ANS_init_blank(&result);
 
-		// preparing result file name
-		strncpy(result_file_name, file_name, strlen(file_name));
-		result_file_name[strlen(file_name)] = '\0';
+		fprintf(output_file, "%c", fgetc(input_file));
+		fprintf(output_file, "%c", fgetc(input_file));
 
-		strcat(file_name, ".txt\0");\
-		strcat(result_file_name, "_result.txt\0");
-
-		// open files
-		ifile = fopen(file_name, "r");
-
-		if (ifile == NULL)
+		// read 2 numbers
+		temp = fgetc(input_file);
+		while ((temp >= '0' && temp <= '9') || (temp >= 'A' && temp <= 'F') || (temp >= 'a' && temp <= 'f'))
 		{
-			file_error = true;
-			printf("Calculator: File does'n exit.\n");
+			fprintf(output_file, "%c", temp);
+			ANS_push_front(&num1, temp);
+			temp = fgetc(input_file);
 		}
+		ANS_reverse(&num1);
+		ANS_fix(&num1);
+		fprintf(output_file, "%c", fgetc(input_file));
 
-		if (!file_error)
+		fprintf(output_file, "\n");
+
+		temp = fgetc(input_file);
+		while ((temp >= '0' && temp <= '9') || (temp >= 'A' && temp <= 'F') || (temp >= 'a' && temp <= 'f'))
 		{
-			ofile = fopen(result_file_name, "w");
+			fprintf(output_file, "%c", temp);
+			ANS_push_front(&num2, temp);
+			temp = fgetc(input_file);
+		}
+		ANS_reverse(&num2);
+		ANS_fix(&num2);
+		fgetc(input_file);
+		fgetc(input_file);
+		fprintf(output_file, "\n\n");
 
-			printf("Calculator: Input file -> %s\n", file_name);
-			printf("Calculator: Output file -> %s\n", result_file_name);
 
-			operation = fgetc(ifile);
-			do
+		if (numeral_system > 16 || numeral_system < 2)
+		{
+			strcpy(operation_error_msg, "ERROR: Invalid numeral system");
+			operation_error = true;
+		}
+		else
+		{
+			for (int i = 0; i < num1.size; i++)
 			{
-				// initialize error flag
-				operation_error = false;
-
-				printf("Calculator: Working on operation %d...", operation_counter);
-
-				// get current operation
-				fprintf(ofile, "%c", operation);
-
-				// arithmetic operations
-				if (operation == '+' || operation == '*' || operation == '/' || operation == '%' || operation == '^')
+				int digit = ANS_chr_toint(ANS_getat(&num1, i));
+				if (!(digit >= 0 && digit < num1.numeral_system))
 				{
-					fprintf(ofile, "%c", fgetc(ifile));
-
-					fscanf(ifile, "%d", &numeral_system1);
-					fprintf(ofile, "%d", numeral_system1);
-
-					// init number containers(allocate memory)
-					ANS_init_cap(&num1, 2, numeral_system1);
-					ANS_init_cap(&num2, 2, numeral_system1);
-					ANS_init_blank(&result);
-
-					fprintf(ofile, "%c", fgetc(ifile));
-					fprintf(ofile, "%c", fgetc(ifile));
-
-					// read 2 numbers
-					c = fgetc(ifile);
-					while ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))
-					{
-						fprintf(ofile, "%c", c);
-						ANS_push_front(&num1, c);
-						c = fgetc(ifile);
-					}
-					ANS_reverse(&num1);
-					ANS_fix(&num1);
-					fprintf(ofile, "%c", fgetc(ifile));
-
-					fprintf(ofile, "\n");
-
-					c = fgetc(ifile);
-					while ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))
-					{
-						fprintf(ofile, "%c", c);
-						ANS_push_front(&num2, c);
-						c = fgetc(ifile);
-					}
-					ANS_reverse(&num2);
-					ANS_fix(&num2);
-					fgetc(ifile);
-					fgetc(ifile);
-					fprintf(ofile, "\n\n");
-
-					// check errors
-					if (numeral_system1 > 16 || numeral_system1 < 2)
-					{
-						strcpy(operation_error_msg, "ERROR: Invalid numeral system");
-						operation_error = true;
-					}
-					else
-					{
-						for (int i = 0; i < num1.size; i++)
-						{
-							int digit = ANS_chr_toint(ANS_getat(&num1, i));
-							if (!(digit >= 0 && digit < num1.numeral_system))
-							{
-								strcpy(operation_error_msg, "ERROR: First number contains invalid digit(s).");
-								operation_error = true;
-							}
-						}
-
-						if (!operation_error)
-						{
-							for (int i = 0; i < num2.size; i++)
-							{
-								int digit = ANS_chr_toint(ANS_getat(&num2, i));
-								if (!(digit >= 0 && digit < num2.numeral_system))
-								{
-									strcpy(operation_error_msg, "ERROR: Second number contains invalid digit(s).");
-									operation_error = true;
-								}
-							}
-						}
-						if (!operation_error)
-						{
-							if (num2.size == 1 && ANS_getat(&num2, 0) == '0' && (operation == '/' || operation == '%'))
-							{
-								strcpy(operation_error_msg, "ERROR: Can't divide by 0");
-								operation_error = true;
-							}
-						}
-					}
-
-					// do operations
-					clock_start = clock();
-					if (!operation_error)
-					{
-						if (operation == '+')
-							ANS_sum_withc(&num1, &num2, &result);
-						else if (operation == '*')
-							ANS_mult_withc(&num1, &num2, &result);
-						else if (operation == '/')
-							ANS_divide_withc(&num1, &num2, &result);
-						else if (operation == '%')
-							ANS_modulo_withc(&num1, &num2, &result);
-						else
-							ANS_pow_withc(&num1, &num2, &result);
-					}
-					clock_end = clock();
-
-					elapsed_time = (float)(clock_end - clock_start) / CLOCKS_PER_SEC;
-					printf(" elapsed time: %f seconds.\n", elapsed_time);
-
-					// print result to the result file
-					if(!operation_error)
-						for (int i = result.size - 1; i >= 0; i--)
-							fprintf(ofile, "%c", result.string[i]);
-					else
-						fprintf(ofile, "%s", operation_error_msg);
-
-					fprintf(ofile, "\n\n");
-
-					// free memory
-					ANS_delete(&num1);
-					ANS_delete(&num2);
-					ANS_delete(&result);
-				}
-				// number conversions
-				else if (operation - '0' >= 0 && operation - '0' <= 9)
-				{
-
-					// get numeral systems
-					c = fgetc(ifile);
-
-					if (c != ' ')
-					{
-						fprintf(ofile, "%c", c);
-						numeral_system1 = 10 + (c - '0');
-						fgetc(ifile);
-					}
-					else
-						numeral_system1 = (operation - '0');
-
-					// init number container(allocate memory)
-					ANS_init_cap(&num, 2, numeral_system1);
-
-					fprintf(ofile, " ");
-					fscanf(ifile, "%d", &numeral_system2);
-					fprintf(ofile, "%d", numeral_system2);
-					fgetc(ifile);
-					fgetc(ifile);
-					fprintf(ofile, "\n\n");
-
-					c = fgetc(ifile);
-
-					// read given number
-					while ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))
-					{
-						fprintf(ofile, "%c", c);
-						ANS_push_front(&num, c);
-						c = fgetc(ifile);
-					}
-					ANS_reverse(&num);
-					ANS_fix(&num);
-					fgetc(ifile);
-					fprintf(ofile, "\n\n");
-
-					// convert the number to the correct numeral system
-
-					// error checking
-					if (numeral_system1 > 16 || numeral_system1 < 2 || numeral_system2 > 16 || numeral_system2 < 2)
-					{
-						strcpy(operation_error_msg, "ERROR: Invalid numeral system");
-						operation_error = true;
-					}
-					else
-					{
-						for (int i = 0; i < num1.size; i++)
-						{
-							int digit = ANS_chr_toint(ANS_getat(&num1, i));
-							if (!(digit >= 0 && digit < num1.numeral_system))
-							{
-								strcpy(operation_error_msg, "ERROR: First number contains invalid digit(s).");
-								operation_error = true;
-							}
-						}
-					}
-
-					// conversion
-					clock_start = clock();
-					if(!operation_error)
-						ANS_convert(&num, numeral_system2);
-					clock_end = clock();
-
-					elapsed_time = (float)(clock_end - clock_start) / CLOCKS_PER_SEC;
-					printf(" elapsed time: %f seconds.\n", elapsed_time);
-
-					// print the result or error message
-					if (!operation_error)
-						for (int i = num.size - 1; i >= 0; i--)
-							fprintf(ofile, "%c", num.string[i]);
-					else
-						fprintf(ofile, "%s", operation_error_msg);
-
-					fprintf(ofile, "\n\n");
-					fgetc(ifile);
-
-					// free memory
-					ANS_delete(&num);
-				}
-				else
-				{
+					strcpy(operation_error_msg, "ERROR: First number contains invalid digit(s).");
 					operation_error = true;
-					fprintf(ofile, "\n");
-					strcpy(operation_error_msg, "ERROR: Invalid operation");
-					fprintf(ofile, "%s", operation_error_msg);
-					printf("\nCalculator: Invalid operation sign interrupted further operation.\n");
-					// incorrect operation breaks whole loop
-					break;
 				}
-				// increment operation counter
-				operation_counter++;
+			}
 
-				operation = fgetc(ifile);
-			} while (operation != EOF);
-
-			// close files and clear strings
-			fclose(ifile);
-			fclose(ofile);
+			if (!operation_error)
+			{
+				for (int i = 0; i < num2.size; i++)
+				{
+					int digit = ANS_chr_toint(ANS_getat(&num2, i));
+					if (!(digit >= 0 && digit < num2.numeral_system))
+					{
+						strcpy(operation_error_msg, "ERROR: Second number contains invalid digit(s).");
+						operation_error = true;
+					}
+				}
+			}
+			if (!operation_error)
+			{
+				if (num2.size == 1 && ANS_getat(&num2, 0) == '0' && (current_operation == '/' || current_operation == '%'))
+				{
+					strcpy(operation_error_msg, "ERROR: Can't divide by 0");
+					operation_error = true;
+				}
+			}
 		}
-		memset(file_name, 0, MAX_FILE_NAME_SIZE);
-		memset(result_file_name, 0, MAX_FILE_NAME_SIZE);
+
+		// do operations
+		clock_start = clock();
+		if (!operation_error)
+		{
+			if (current_operation == '+')
+				ANS_sum_withc(&num1, &num2, &result);
+			else if (current_operation == '*')
+				ANS_mult_withc(&num1, &num2, &result);
+			else if (current_operation == '/')
+				ANS_divide_withc(&num1, &num2, &result);
+			else if (current_operation == '%')
+				ANS_modulo_withc(&num1, &num2, &result);
+			else
+				ANS_pow_withc(&num1, &num2, &result);
+		}
+		clock_end = clock();
+
+		elapsed_time = (float)(clock_end - clock_start) / CLOCKS_PER_SEC;
+		printf(" elapsed time: %f seconds.\n", elapsed_time);
+
+		// print result to the result file
+		if (!operation_error)
+			for (int i = result.size - 1; i >= 0; i--)
+				fprintf(output_file, "%c", result.string[i]);
+		else
+			fprintf(output_file, "%s", operation_error_msg);
+
+		fprintf(output_file, "\n\n");
+
+		// free memory
+		ANS_delete(&num1);
+		ANS_delete(&num2);
+		ANS_delete(&result);
 	}
-	return 0;
+	// number conversions
+	else if (current_operation - '0' >= 0 && current_operation - '0' <= 9)
+	{
+
+		// get numeral systems
+		temp = fgetc(input_file);
+
+		if (temp != ' ')
+		{
+			fprintf(output_file, "%c", temp);
+			numeral_system = 10 + (temp - '0');
+			fgetc(input_file);
+		}
+		else
+			numeral_system = (current_operation - '0');
+
+		// init number container(allocate memory)
+		ANS_init_cap(&num, 2, numeral_system);
+
+		fprintf(output_file, " ");
+		fscanf(input_file, "%d", &numeral_system2);
+		fprintf(output_file, "%d", numeral_system2);
+		fgetc(input_file);
+		fgetc(input_file);
+		fprintf(output_file, "\n\n");
+
+		temp = fgetc(input_file);
+
+		// read given number
+		while ((temp >= '0' && temp <= '9') || (temp >= 'A' && temp <= 'F') || (temp >= 'a' && temp <= 'f'))
+		{
+			fprintf(output_file, "%c", temp);
+			ANS_push_front(&num, temp);
+			temp = fgetc(input_file);
+		}
+		ANS_reverse(&num);
+		ANS_fix(&num);
+		fgetc(input_file);
+		fprintf(output_file, "\n\n");
+
+		// convert the number to the correct numeral system
+
+		// error checking
+		if (numeral_system > 16 || numeral_system < 2 || numeral_system2 > 16 || numeral_system2 < 2)
+		{
+			strcpy(operation_error_msg, "ERROR: Invalid numeral system");
+			operation_error = true;
+		}
+		else
+		{
+			for (int i = 0; i < num.size; i++)
+			{
+				int digit = ANS_chr_toint(ANS_getat(&num, i));
+				if (!(digit >= 0 && digit < num.numeral_system))
+				{
+					strcpy(operation_error_msg, "ERROR: Number contains invalid digit(s).");
+					operation_error = true;
+				}
+			}
+		}
+
+		// conversion
+		clock_start = clock();
+		if (!operation_error)
+			ANS_convert(&num, numeral_system2);
+		clock_end = clock();
+
+		elapsed_time = (float)(clock_end - clock_start) / CLOCKS_PER_SEC;
+		printf(" elapsed time: %f seconds.\n", elapsed_time);
+
+		// print the result or error message
+		if (!operation_error)
+			for (int i = num.size - 1; i >= 0; i--)
+				fprintf(output_file, "%c", num.string[i]);
+		else
+			fprintf(output_file, "%s", operation_error_msg);
+
+		fprintf(output_file, "\n\n");
+		fgetc(input_file);
+
+		// free memory
+		ANS_delete(&num);
+	}
+	else
+	{
+		operation_error = true;
+		fprintf(output_file, "\n");
+		strcpy(operation_error_msg, "ERROR: Invalid operation");
+		fprintf(output_file, "%s", operation_error_msg);
+		printf("\nCalculator: Invalid operation sign interrupted further operation.\n");
+		// incorrect operation breaks whole loop
+		return false;
+	}
+
+	return true;
+}
+
+void create_current_output_file_name(char* input_file_path, char* output_file_path_container, int operation_counter)
+{
+	char result[MAX_FILE_NAME_SIZE];
+	char number[MAX_FILE_NAME_SIZE];
+	char temp;
+	int temp_digit;
+	int size;
+
+	// getting file name without extension and saving it in result container
+	strncpy(result, input_file_path, strlen(input_file_path) - 4);
+	result[strlen(input_file_path) - 4] = '\0';
+
+	strcat(result, "_result[\0");
+
+	size = 0;
+	do
+	{
+		temp_digit = operation_counter % 10;
+		number[size++] = temp_digit + '0';
+	} while (operation_counter /= 10);
+
+	number[size] = '\0';
+
+	for (int i = 0, j = size - 1; i < j; i++, j--)
+	{
+		temp = number[i];
+		number[i] = number[j];
+		number[j] = temp;
+	}
+
+	strcat(result, number);
+
+	strcat(result, "].txt\0");
+
+	strncpy(output_file_path_container, result, strlen(result));
+	output_file_path_container[strlen(result)] = '\0';
 }
